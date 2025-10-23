c562f138621db0e0cdb863e7fd9a5bb294d68cfb
Yoe <dembouz719@gmail.com>
2025-10-23 18:48:21 +0200

    testar kod för att kolla om det går att göra en review

--- backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello World!");
+		System.out.println("Hello Sweden!");
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ange ditt namn: ");
+		String input = scanner.nextLine();
+		System.out.println("Hej " + input + "!");
+		scanner.close();
 	}
 
 }
