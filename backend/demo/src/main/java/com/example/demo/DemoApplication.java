c5fbda81d5bb0b26a279295d518de753e6e81cb6
Yoe <dembouz719@gmail.com>
2025-11-05 18:08:58 +0100

    Added start function for SpringBoot

--- backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,27 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+import com.example.demo.service.MongoConnectionService;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// Start Spring Boot application
+		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		
+		// Print messages after Spring Boot starts
 		System.out.println("Hello World!");
 		System.out.println("Hello Sweden!");
-		
 		System.out.println("Hello India!");
+		
+		// Test MongoDB connection
+		MongoConnectionService mongoService = context.getBean(MongoConnectionService.class);
+		mongoService.testConnection();
 	}
 
 }
