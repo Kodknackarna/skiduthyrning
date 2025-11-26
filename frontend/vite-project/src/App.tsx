17e96ae375ba9acc2f20f3c1d25e32f068f6127f
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2025-11-26 18:57:44 +0100

    Created the start of frontend and a component that calls on the newly created  Controller in the backend

---
+++ frontend/vite-project/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { ShowCustomers } from './Components/showCustomers'
+
+function App() {
+
+  return (
+    <>
+      <h1>Skiduthyrning</h1>
+      <ShowCustomers />
+    </>
+  )
+}
+
+export default App
